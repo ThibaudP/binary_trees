0925ccea62bdc7bdee5f160cb473f433429354af
ThibaudP <t.poncin@gmail.com>
2021-03-08 13:09:36 +0000

    fix(): formatting

--- binary_trees.h
+++ binary_trees.h
@@ -17,6 +17,7 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
+
 struct binary_tree_s
 {
 	int n;
@@ -24,10 +25,16 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
 
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *tree);
