ad344e4d57ebc9ff256e8873002bb3d14a5a076e
Natalia Ponsard <2503@holbertonschool.com>
2021-03-11 16:52:51 +0000

    tasks 22, 24

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - a left-rotation on a binary tree
+ *
+ * @tree: a pointer to the root node
+ *
+ * Return: a pointer to the new root node of the tree once rotated
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *piv1, *piv2;
+
+	piv1 = tree->parent;
+	piv2 = tree->right;
+	tree->right = piv2->left;
+
+	if (piv2->left != NULL)
+		piv2->left->parent = tree;
+
+	piv2->left = tree;
+	tree->parent = piv2;
+	piv2->parent = piv1;
+
+	if (piv1 != NULL)
+	{
+		if (piv1->right == tree)
+			piv1->right = piv2;
+		else
+			piv1->left = piv2;
+	}
+	return (piv2);
+}
---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,47 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leftcheck - check if all values greater than a value
+ *
+ * @tree: a pointer to the root node
+ *
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ */
+int binary_tree_check(const binary_tree_t *tree, int value)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (1);
+
+	if ((tree->n > value) || (tree->n < value))
+	{
+		left = binary_tree_check(tree->left, value);
+		right = binary_tree_check(tree->right, value);
+		if (left && right)
+			return (1);
+	}
+	return (0);
+}
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
+ *
+ * @tree: a pointer to the root node
+ *
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ */
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if ((binary_tree_check(tree->left, tree->n)) && (binary_tree_check(tree->right, tree->n)))
+	{
+		if ((tree->left == NULL) || (binary_tree_is_bst(tree->left)))
+		{
+			if ((tree->right == NULL) || (binary_tree_is_bst(tree->right)))
+				return (1);
+		}
+	}
+	return (0);
+}
