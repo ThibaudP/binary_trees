e2c8305cffcb396c1bfd1edc44c903d55590ce44
ThibaudP <t.poncin@gmail.com>
2021-03-11 19:43:32 +0000

    fix(): fix algo in 110

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,30 +1,58 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_check - check if all values greater than a value
+ * tree_greater_than - Checks if all nodes of a tree is greater than a value
  *
- *@value: a value to compare
- *@tree: a pointer to the root node
+ * @tree: root node of the tree
+ * @val: value to check against
  *
- * Return: 1 if tree is a valid BST, and 0 otherwise
+ * Return: 1 if true, 0 if false
  */
 
-int binary_tree_check(const binary_tree_t *tree, int value)
+int tree_greater_than(const binary_tree_t *tree, int val)
 {
 	int left, right;
 
 	if (tree == NULL)
 		return (1);
-
-	if ((tree->n > value) || (tree->n < value))
+	if (tree->n > val)
 	{
-		left = binary_tree_check(tree->left, value);
-		right = binary_tree_check(tree->right, value);
+		left = tree_greater_than(tree->left, val);
+		right = tree_greater_than(tree->right, val);
 		if (left && right)
 			return (1);
 	}
+
 	return (0);
 }
+
+/**
+ * tree_lower_than - Checks if all nodes of a tree is lower than a value
+ *
+ * @tree: root node of the tree
+ * @val: value to check against
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int tree_lower_than(const binary_tree_t *tree, int val)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (1);
+	if (tree->n < val)
+	{
+		left = tree_lower_than(tree->left, val);
+		right = tree_lower_than(tree->right, val);
+		if (left && right)
+			return (1);
+	}
+
+	return (0);
+}
+
+
 
 /**
  * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
@@ -39,8 +67,8 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if ((binary_tree_check(tree->left, tree->n)) &&
-	    (binary_tree_check(tree->right, tree->n)))
+	if ((tree_lower_than(tree->left, tree->n)) &&
+	    (tree_greater_than(tree->right, tree->n)))
 	{
 		if ((tree->left == NULL) || (binary_tree_is_bst(tree->left)))
 		{
